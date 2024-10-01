c04e5d28f5203461437282f9fbdeeec992d92227
Ziaurrehm <irfankhan@saeedantechpvt.com>
2024-10-01 17:22:19 +0500

    Header and footer added

--- src/Views/Landing/components/Footer/Footer.jsx
+++ src/Views/Landing/components/Footer/Footer.jsx
@@ -1,15 +1,74 @@
-import { Box, styled } from '@mui/material'
-import React from 'react'
-const StyledFooter = styled(Box)(({theme})=> ({
-    height:'30vh',
-    background:theme.palette.primary.main
-}))
+
+
+import { Box, Grid, Typography, Divider, styled } from "@mui/material";
+import { Facebook, YouTube } from "@mui/icons-material";
+import React from "react";
+
+const StyledFooter = styled(Box)(({ theme }) => ({
+  backgroundColor: "#000",
+  color: "#fff",
+  padding: "2rem 1rem",
+  [theme.breakpoints.down("sm")]: {
+    textAlign: "left",
+  },
+}));
+
 const Footer = () => {
   return (
     <StyledFooter>
-      This is Footer
-    </StyledFooter>
-  )
-}
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Typography variant="h6">Your Logo</Typography>
+          <Typography variant="body2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit.
+          </Typography>
+          <Box mt={2} sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Box display="flex" alignItems="center" gap={1}>
+              <Facebook />
+              <Typography variant="body2">Follow us on Facebook</Typography>
+            </Box>
+            <Box display="flex" alignItems="center" gap={1} mt={1}>
+              <YouTube />
+              <Typography variant="body2">Follow us on YouTube</Typography>
+            </Box>
+          </Box>
+        </Grid>
 
-export default Footer
+        <Grid item xs={12} sm={6} md={2}>
+          <Typography variant="h6">Menu</Typography>
+          <Box mt={2} display="flex" flexDirection="column" gap={1}>
+            <Typography variant="body2">How to Play</Typography>
+            <Typography variant="body2">FAQ's</Typography>
+            <Typography variant="body2">Products</Typography>
+          </Box>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={3}>
+          <Typography variant="h6">About Virtual Credit Card</Typography>
+          <Box mt={2} display="flex" flexDirection="column" gap={1}>
+            <Typography variant="body2">Where to use Ezzocard VCCs</Typography>
+            <Typography variant="body2">Blog</Typography>
+          </Box>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={3}>
+          <Box mt={2} display="flex" flexDirection="column" gap={1}>
+            <Typography variant="h6">Contact Us</Typography>
+            <Typography variant="body2">+92-343-6683-499</Typography>
+            <Typography variant="body2">abc@gmail.com</Typography>
+            <Typography variant="body2">92, Saeedan, Lahore</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Divider style={{ backgroundColor: "#fff", margin: "2rem 0" }} />
+
+      <Typography align="center" variant="body2">
+        Ezzocard 2024 All rights reserved.
+      </Typography>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
--- src/Views/Landing/components/Header/Header.jsx
+++ src/Views/Landing/components/Header/Header.jsx
@@ -1,20 +1,77 @@
-import { AppBar, Button, Toolbar } from '@mui/material'
-import React from 'react'
+import { AppBar, Toolbar, Button, Typography, Box, IconButton, Divider } from '@mui/material';
+import { ShoppingCart, Menu as MenuIcon, KeyboardArrowDown } from '@mui/icons-material'; 
+import React from 'react';
+import imagelog from "../../../../assets/images/logo.png";
 
 const Header = () => {
   return (
-    <AppBar position='static'>
-    <Toolbar>
-        Landing
-        <Button  variant='contained' sx={{ml:'auto'}}>
-        Login
-        </Button> 
-        <Button variant='contained'>
-            SignUpfefwewrewr
-        </Button>
-    </Toolbar>
-</AppBar>
-  )
-}
+    <AppBar position="static" sx={{ backgroundColor: '#fff', color: '#000', padding: '0 1rem' }}>
+      <Toolbar>
 
-export default Header
+        <Box display="flex" alignItems="center">
+          <Typography variant="h6">
+            <img src={imagelog} alt="logo" style={{ height: '40px' }} />
+          </Typography>
+          <Divider orientation="vertical" flexItem sx={{ height: '40px', margin: '0 16px' }} />
+        </Box>
+
+      
+        <Box display="flex" alignItems="center" gap={2} sx={{ flexGrow: 1 }}>
+          <Box display="flex" alignItems="center">
+            <Typography variant="body1">Prepaid Cards</Typography>
+            <KeyboardArrowDown />
+          </Box>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: '#8AE700',
+              color: '#000',
+              '&:hover': { backgroundColor: '#76c300' },
+            }}
+          >
+            Go to Gift Cards
+          </Button>
+        </Box>
+
+     
+        <Box display="flex" alignItems="center" gap={2}>
+          <Box display="flex" alignItems="center">
+            <Typography variant="body1">Registration Balance</Typography>
+            <KeyboardArrowDown />
+          </Box>
+
+          <IconButton edge="end" color="inherit">
+            <MenuIcon />
+          </IconButton>
+
+          <Box display="flex" alignItems="center">
+            <Typography variant="body1">How to Pay</Typography>
+            <KeyboardArrowDown />
+          </Box>
+          
+          <Box display="flex" alignItems="center">
+            <Typography variant="body1">More</Typography>
+            <KeyboardArrowDown />
+          </Box>
+
+          <IconButton edge="end" color="inherit">
+            <ShoppingCart />
+          </IconButton>
+
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: '#8AE700',
+              color: '#000',
+              '&:hover': { backgroundColor: '#76c300' },
+            }}
+          >
+            Buy Prepaid Cards
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
