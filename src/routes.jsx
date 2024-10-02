b669837f5fbd1f50c21470068b2e887382ee107d
Ziaurrehm <irfankhan@saeedantechpvt.com>
2024-10-02 11:55:51 +0500

    changes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import { useRoutes } from "react-router-dom/dist";
 import Landing from "./Views/Landing/Landing";
+import ConfirmOrders from "./Views/Landing/components/Content/OrderPages/ConfirmOrders";
 
 
 export default function Router() {
@@ -8,7 +9,10 @@ export default function Router() {
         path:'/',
         element : <Landing /> ,
        },
-       
+       {
+        path:'/confirm-orders',
+        element : <ConfirmOrders /> ,
+       },
     ])
     return element
 }
